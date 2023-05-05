014023964a03678519f59c84854834e4cc2d855a
김남진 <skawls1938@gmail.com>
2023-05-05 20:32:40 +0900

    pdftext pasing & blank  try
    pdftext pasing & blank  try

--- board_exam_project-master/src/main/java/com/study/board/BoardApplication.java
+++ board_exam_project-master/src/main/java/com/study/board/BoardApplication.java
@@ -1,13 +1,32 @@
 package com.study.board;
 
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import kr.co.shineware.nlp.komoran.model.KomoranResult;
+import kr.co.shineware.nlp.komoran.model.Token;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 
 @SpringBootApplication
 public class BoardApplication {
 
 	public static void main(String[] args) {
+
+		Komoran komoran = new Komoran(DEFAULT_MODEL.FULL);
+		String strToAnalyze = "대한민국은 민주공화국이다.";
+
+		KomoranResult analyzeResultList = komoran.analyze(strToAnalyze);
+
+		System.out.println(analyzeResultList.getPlainText());
+
+		List<Token> tokenList = analyzeResultList.getTokenList();
+		for (Token token : tokenList) {
+			System.out.format("(%2d, %2d) %s/%s\n", token.getBeginIndex(), token.getEndIndex(), token.getMorph(), token.getPos());
+		}
+
 
 		SpringApplication.run(BoardApplication.class, args);
 	}
