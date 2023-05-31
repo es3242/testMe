80db3df06ced5fe6b4d30382fa18b0eeb0f69857
Raa <say0049@naver.com>
2023-05-31 22:28:06 +0900

    230530 수정사항 업로드

--- board_exam_project-master/src/main/java/com/study/board/controller/PdftextController.java
+++ board_exam_project-master/src/main/java/com/study/board/controller/PdftextController.java
@@ -1,11 +1,14 @@
 package com.study.board.controller;
 
-
+import com.study.board.service.PdfService;
 import org.apache.http.HttpResponse;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.*;
@@ -19,78 +22,88 @@ import org.apache.http.client.methods.HttpPost;
 
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
-//
 @Controller
 public class PdftextController {
+
+    @Autowired
+    private PdfService pdfService;
+
 
     @GetMapping("/pdftext")
     public String pdftext() {
         Random random = new Random();
         PDDocument document = null;
-        String path = "C:\\asdasd\\test3.pdf"; // PDF 파일 경로
-        String outputFilePath = "C:\\asdasd\\test3.txt"; // 출력 텍스트 파일 경로
+        String path = "./pdf/test3.pdf"; // PDF file path
+        String outputFilePath = "./pdf/test3.txt"; // output text file path
 
         try {
-            document = PDDocument.load(new File(path)); // PDF 파일 로드
-            PDFTextStripper stripper = new PDFTextStripper(); // PDF 텍스트 추출기
-            int numPages = document.getNumberOfPages(); // 페이지 수 확인
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath)); // 텍스트 파일 작성기
+            document = PDDocument.load(new File(path)); // load PDF file
+            PDFTextStripper stripper = new PDFTextStripper(); // PDF text extractor
+            int numPages = document.getNumberOfPages(); // check the number of pages
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath)); // text file writer
 
-            writer.write("Number of pages: " + numPages + "\n"); // 페이지 수를 파일에 작성
+            writer.write("Number of pages: " + numPages + "\n"); // write page count to file
+
+            JSONArray jsonArray = new JSONArray(); // create JSONArray for all pages
 
             for (int pageNum = 1; pageNum <= numPages; pageNum++) {
-                stripper.setStartPage(pageNum); // 시작 페이지 설정
-                stripper.setEndPage(pageNum); // 끝 페이지 설정
-                String pageText = stripper.getText(document); // 페이지에서 텍스트 추출
+                stripper.setStartPage(pageNum); // set start page
+                stripper.setEndPage(pageNum); // set end page
+                String pageText = stripper.getText(document); // extract text from page
 
-                writer.write("Processed text from page " + pageNum + ":\n"); // 페이지 번호를 파일에 작성
-                writer.write(pageText); // 처리된 텍스트를 파일에 작성
-
-                writer.write("\nOriginal text:\n"); // 원본 텍스트를 파일에 작성
-                writer.write(pageText); // 원본 텍스트를 파일에 작성
+                writer.write("Processed text from page " + pageNum + ":\n"); // write the page numbers to the file
+                writer.write(pageText); // Write the processed text to a file
+                writer.write("\nOriginal text:\n"); // write original text to file
+                writer.write(pageText); // write original text to file
                 writer.write("\n");
 
-                // JSON 데이터 생성
-                JSONObject json = new JSONObject();
-                json.put("text", pageText);
-                json.put("page", pageNum);
+                // create JSON object for the page
+                JSONObject pageJson = new JSONObject();
+                pageJson.put("text", pageText);
 
-                try {
-                    // HTTP 연결 설정
-                    URL url = new URL("http://localhost:5000/endpoint");
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("POST");
-                    connection.setRequestProperty("Content-Type", "application/json");
-                    connection.setDoOutput(true);
 
-                    // JSON 데이터를 연결의 출력 스트림에 작성
-                    OutputStream outputStream = connection.getOutputStream();
-                    BufferedWriter httpWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
-                    httpWriter.write(json.toString());
-                    httpWriter.flush();
-                    httpWriter.close();
-
-                    // 서버로부터 응답 읽기
-                    int responseCode = connection.getResponseCode();
-                    if (responseCode == HttpURLConnection.HTTP_OK) {
-                        // 요청이 성공적으로 처리됨
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                        String response = reader.readLine();
-                        reader.close();
-                        System.out.println("Server response: " + response);
-                    } else {
-                        // 오류 응답 처리
-                        System.out.println("Error response from server. Response Code: " + responseCode);
-                    }
-
-                    connection.disconnect();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                // Add the JSON object to the JSONArray
+                jsonArray.put(pageJson);
             }
+
             writer.close();
+
+            try {
+                // set up HTTP connection
+                URL url = new URL("http://localhost:5000/endpoint");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setDoOutput(true);
+
+                // Write JSON data to the connection's output stream
+                OutputStream outputStream = connection.getOutputStream();
+                BufferedWriter httpWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+                httpWriter.write(jsonArray.toString());
+                httpWriter.flush();
+                httpWriter.close();
+
+                // read response from server
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // request processed successfully
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String response = reader.readLine();
+                    reader.close();
+                    System.out.println("Server response: " + response);
+                } else {
+                    // handle error response
+                    System.out.println("Error response from server. Response Code: " + responseCode);
+                }
+
+                connection.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -108,4 +121,26 @@ public class PdftextController {
 
 
 
+
+
+
+    @GetMapping("/saveinfo")
+    public String getQuestionInfo(Model model) {
+        model.addAttribute("page", 1); // 초기 페이지를 1로 설정
+        model.addAttribute("info", ""); // 초기 정보를 빈 문자열로 설정
+        return "pdf/createquestion";
+    }
+
+    @PostMapping("/saveinfo")
+    public String saveInfo(@RequestParam("page") int page, @RequestParam("info") String info) {
+        // 사용자 입력을 처리하고 필요에 따라 정보를 저장합니다
+        System.out.println("페이지: " + page + ", 정보: " + info);
+
+        // 정보를 서버로 전송합니다
+        JSONObject json = new JSONObject();
+        json.put("text", info);
+        json.put("page", page);
+        pdfService.sendInfoToServer(json);
+        return "redirect:/pdftext"; // 메인 페이지로 리디렉션합니다
+    }
 }
--- board_exam_project-master/src/main/java/com/study/board/service/PdfService.java
+++ board_exam_project-master/src/main/java/com/study/board/service/PdfService.java
@@ -4,13 +4,15 @@ import com.study.board.entity.Pdf;
 import com.study.board.entity.User;
 import com.study.board.repository.PdfRepository;
 import com.study.board.repository.UserRepository;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -34,6 +36,37 @@ public class PdfService {
     public List<Pdf> getPdfListByUserId(Long userId) {
         User user = userRepository.findById(userId).orElse(null);
         return pdfRepository.findByUserOrderByCreatedAtDesc(user);
+    }
+
+    public void sendInfoToServer(JSONObject json) {
+        try {
+            // 외부 서버의 주소나 엔드포인트에 맞게 URL을 수정합니다
+            URL url = new URL("http://external-server.com/endpoint");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+
+            OutputStream outputStream = connection.getOutputStream();
+            BufferedWriter httpWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+            httpWriter.write(json.toString());
+            httpWriter.flush();
+            httpWriter.close();
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String response = reader.readLine();
+                reader.close();
+                System.out.println("Server response: " + response);
+            } else {
+                System.out.println("Error response from server. Response Code: " + responseCode);
+            }
+
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
